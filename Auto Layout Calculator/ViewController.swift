2b569cdd38a8fb7a0164e0dc3cdb2a3b506055dd
maxesse <max@maxsanna.com>
2018-02-13 17:42:50 +0000

    Refactored code with functions and reduced repetitions. Added app icons

--- Auto Layout Calculator/ViewController.swift
+++ Auto Layout Calculator/ViewController.swift
@@ -11,12 +11,15 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var calculatorDisplay: UILabel!
     
+    // Setting class variables
+    
     var runningTotal: Double = 0
     var lastOperation: String = ""
     var operand: Double = 0
     var displayLabelText: String = "0"
     var displayResetFlag = false
     var negativeFlag = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         calculatorDisplay.text = displayLabelText
@@ -30,15 +33,21 @@ class ViewController: UIViewController {
 
 
     @IBAction func buttonPressed(_ sender: UIButton) {
+        // This statement resets the label if you press an operation key so you can enter the second value
+        
         if lastOperation != "" && displayResetFlag == false {
             displayLabelText = "0"
             displayResetFlag = true
         }
+        
+        // We evaluate and do different things for each key pressed
         switch sender.tag {
+        
         case 0:                                                 // 0
             if displayLabelText != "0" {
-                displayLabelText = "\(displayLabelText)0"
+                displayLabelText = "\(displayLabelText)\(sender.tag)"
             }
+        
         case 1,2,3,4,5,6,7,8,9,10:                              // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
             if displayLabelText == "0" {
                 displayLabelText = "\(sender.tag)"
@@ -47,57 +56,13 @@ class ViewController: UIViewController {
             }
         
         case 20:                                                // Addition
-            if lastOperation == "" {
-                if let displayLabelDouble = Double(displayLabelText) {
-                    runningTotal = displayLabelDouble
-                }
-            } else {
-                if let displayLabelDouble = Double(displayLabelText) {
-                    operand = displayLabelDouble
-                }
-                recalculate()
-            }
-            displayResetFlag = false
-            lastOperation = "+"
+            operation(selectedOperationKey: "+")
         case 21:                                                // Subtraction
-            if lastOperation == "" {
-                if let displayLabelDouble = Double(displayLabelText) {
-                    runningTotal = displayLabelDouble
-                }
-            } else {
-                if let displayLabelDouble = Double(displayLabelText) {
-                    operand = displayLabelDouble
-                }
-                recalculate()
-            }
-            displayResetFlag = false
-            lastOperation = "-"
+            operation(selectedOperationKey: "-")
         case 22:                                                // Multiplication
-            if lastOperation == "" {
-                if let displayLabelDouble = Double(displayLabelText) {
-                    runningTotal = displayLabelDouble
-                }
-            } else {
-                if let displayLabelDouble = Double(displayLabelText) {
-                    operand = displayLabelDouble
-                }
-                recalculate()
-            }
-            displayResetFlag = false
-            lastOperation = "*"
+            operation(selectedOperationKey: "*")
         case 23:                                                // Division
-            if lastOperation == "" {
-                if let displayLabelDouble = Double(displayLabelText) {
-                    runningTotal = displayLabelDouble
-                }
-            } else {
-                if let displayLabelDouble = Double(displayLabelText) {
-                    operand = displayLabelDouble
-                }
-                recalculate()
-            }
-            displayResetFlag = false
-            lastOperation = "/"
+            operation(selectedOperationKey: "/")
         case 24:                                                // Equal
             if let displayLabelDouble = Double(displayLabelText) {
                 operand = displayLabelDouble
@@ -127,18 +92,7 @@ class ViewController: UIViewController {
             }
 
         case 32:                                                // Percentage
-            if lastOperation == "" {
-                if let displayLabelDouble = Double(displayLabelText) {
-                    runningTotal = displayLabelDouble / 100
-                }
-            } else {
-                if let displayLabelDouble = Double(displayLabelText) {
-                    operand = displayLabelDouble / 100
-                }
-                recalculate()
-            }
-            displayResetFlag = false
-            lastOperation = ""
+            operation(selectedOperationKey: "%")
         default:
             print("Sender value out of range")
         }
@@ -161,6 +115,34 @@ class ViewController: UIViewController {
             print("Last operand out of range")
         }
         displayLabelText = String(runningTotal)
+    }
+    
+    func operation(selectedOperationKey: String) {
+        if lastOperation == "" {
+            if let displayLabelDouble = Double(displayLabelText) {
+                if selectedOperationKey != "%" {
+                    runningTotal = displayLabelDouble
+                } else {
+                    runningTotal = displayLabelDouble / 100
+                }
+            }
+        } else {
+            if let displayLabelDouble = Double(displayLabelText) {
+                if selectedOperationKey != "%" {
+                    operand = displayLabelDouble
+                } else {
+                    operand = displayLabelDouble / 100
+                }
+            }
+            recalculate()
+        }
+        displayResetFlag = false
+        if selectedOperationKey == "%" {
+            lastOperation = ""
+        } else {
+            lastOperation = selectedOperationKey
+        }
+
     }
 }
 
